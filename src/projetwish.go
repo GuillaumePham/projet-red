eecf5fa556d74ed86d0feb3cb9dfa8f548720870
Canelle1980 <91125808+Canelle1980@users.noreply.github.com>
2021-09-24 12:22:27 +0200

    Menu

--- src/projetwish.go
+++ src/projetwish.go
@@ -14,8 +14,9 @@ type personnage struct { //creation d'une classe
 	inventaire []string
 	skill      []string
 	money      int
+	equipement equipement
 }
-type equipement *struct {
+type equipement struct {
 	tête  string
 	torse string
 	pied  string
@@ -126,6 +127,7 @@ func (p *personnage) poison() { // retire 30 hp aux personnages
 		fmt.Println(p.nom, ":", p.pvactuel)
 		p.pvactuel = p.pvactuel - 10
 		if p.pvactuel < 0 {
+			time.Sleep(1 * time.Second)
 			p.dead()
 			break
 		}
@@ -146,6 +148,15 @@ func (p *personnage) spellBook(talentcaché string) { //attribue des compétemce
 func (p *personnage) menu() {
 	var commande string
 	fmt.Println("☺----- Bienvenue dans Cristian's dungeon -----☺")
+	fmt.Println()
+	fmt.Println("↓----------------------------------------------↓")
+	fmt.Println("Tapez inventaire si vous voulez voir votre inventaire")
+	fmt.Println("---------------------------------------------------")
+	fmt.Println("Tapez Information pour voir les specs(vie,talent...) de votre personnage")
+	fmt.Println("---------------------------------------------------")
+	fmt.Println("Tapez Marchand pour exercer votre Pouvoirs d'achat ")
+	fmt.Println()
+	fmt.Print("→")
 	fmt.Scan(&commande)
 	switch commande {
 	case "Information":
@@ -156,7 +167,31 @@ func (p *personnage) menu() {
 		p.accessInventory()
 	case "inventaire":
 		p.accessInventory()
-
+	case "Marchand":
+		var marchand int
+		fmt.Println("→ Bienvenue chez Jacquie Farce & Attrape ← ")
+		fmt.Println("Produit en Vente :")
+		fmt.Println("→ Potion de Soins: 3$ (Tapez 0)")
+		fmt.Println("→ Potion de Poison 6$(Tapez 1)")
+		fmt.Println("→ Potion de Livre de Sort Boule de feu  !25 $(Tapez 2)")
+		fmt.Println("→ Fourrure de loup  4$ (Tapez 3")
+		fmt.Println("→ Peau de Troll  7$ (Tapez 4")
+		fmt.Println("→ Peau de Sanglier 3$ (Tapez 5")
+		fmt.Println("→ Plume de Corbeau 1 $ (Tapez 6) ")
+		fmt.Scan(&marchand)
+		p.pnj((marchand))
+	case "marchand":
+		var marchand int
+		fmt.Println("→ Bienvenue chez Jacquie Farce & Attrape ← ")
+		fmt.Println("Produit en Vente :")
+		fmt.Println("→ Potion de Soins: 3$ (Tapez 0)")
+		fmt.Println("→ Potion de Poison 6$(Tapez 1)")
+		fmt.Println("→ Potion de Livre de Sort Boule de feu  !25 $(Tapez 2)")
+		fmt.Println("→ Fourrure de loup  4$ (Tapez 3")
+		fmt.Println("→ Peau de Troll  7$ (Tapez 4")
+		fmt.Println("→ Peau de Sanglier 3$ (Tapez 5")
+		fmt.Scan(&marchand)
+		p.pnj((marchand))
 	}
 }
 func (p *personnage) pnj(i int) { // pnj vendeurs qui vend pas
@@ -175,12 +210,7 @@ func (p *personnage) pnj(i int) { // pnj vendeurs qui vend pas
 		} else {
 			fmt.Println("Plus de Place ☺")
 		}
-
 	}
-	{
-
-	}
-
 	if i == 3 && p.money >= 4 {
 		p.addInventory("Fourrure de loup")
 		p.money = p.money - 4
@@ -198,14 +228,105 @@ func (p *personnage) pnj(i int) { // pnj vendeurs qui vend pas
 		p.money = p.money - 1
 	}
 }
+func (p *personnage) forgeron(b int) {
+	var Chapeau string = "Chapeau de l'aventurier" // crée chapeau qui contient Chapeau de l'aventurier
+	var Tunique string = "Tunique de l'aventurier" // crée tunique qui contient tunique de l'aventurier
+	var Bottes string = "Bottes de l'aventurier"   // crée bottes qui contient bottes de l'aventurier
+	fmt.Println("Pour craft le Chapeau de l'aventurier" + " écriver Chapeau")
+	fmt.Println("Pour craft la Tunique de l'aventurier" + " écriver Tunique")
+	fmt.Println("Pour craft les Bottes de l'aventurier" + " écriver Bottes")
+	var negative int = 0
+	if p.money == negative {
+		fmt.Println("vous n'avez plus d'argent sur vous!")
+	}
+	for i := 0; i <= len(p.inventaire); i++ {
+		if b == 0 && p.money >= 5 {
+			if p.inventaire[i] == "Plume de Corbeau" {
+				if p.inventaire[i] == "Cuir de Sanglier" {
+					p.removeInventory("Plume de Corbeau")
+					p.removeInventory("Cuir de Sanglier")
+					p.money = p.money - 5
+					p.addInventory(Chapeau)
+				}
+			} else if p.money < 5 {
+				fmt.Println("Vous n'avez plus assez d'agent!!")
+				for c := 0; c < p.money; c++ {
+					if p.money < 5 && c > 1 {
+						fmt.Print("Il te reste ") // donne le nombre de po restant du joueur
+						fmt.Print(c)
+						fmt.Print(" pièces d'or")
+
+					} else if p.money < 5 && c <= 1 {
+						fmt.Print("Il te reste ")
+						fmt.Print(c)
+						fmt.Print(" pièce d'or")
+					}
+				}
+			} else {
+				fmt.Println("Il vous manque un item pour craft")
+			}
+		}
+
+		//if Tunique && p.money >= 5 && p.inventaire[i+2] == "Fourrure de loup" && p.inventaire[i] == "Peau de Troll" {
+		if b == 1 && p.money >= 5 {
+			if p.inventaire[i] == "Fourrure de loup"+"Fourrure de loup" {
+				if p.inventaire[i] == "Peau de Troll" {
+					p.removeInventory("Fourrure de loup" + "Fourrure de loup")
+					p.removeInventory("Peau de Troll")
+					p.money = p.money - 5
+					p.addInventory(Tunique)
+				}
+			} else if p.money < 5 {
+				fmt.Println("Vous n'avez plus assez d'agent!!")
+				for c := 0; c < p.money; c++ {
+					if p.money < 5 && c > 1 {
+						fmt.Print("Il te reste ") // donne le nombre de po restant du joueur
+						fmt.Print(c)
+						fmt.Print(" pièces d'or")
+
+					} else if p.money < 5 && c <= 1 {
+						fmt.Print("Il te reste ")
+						fmt.Print(c)
+						fmt.Print(" pièce d'or")
+					}
+				}
+			} else {
+				fmt.Println("Il vous manque un item pour craft")
+			}
+		}
+		if b == 2 && p.money >= 5 {
+			if p.inventaire[i] == "Fourrure de loup" {
+				if p.inventaire[i] == "Peau de Troll" {
+					p.removeInventory("Fourrure de loup")
+					p.removeInventory("Cuir de Sanglier")
+					p.money = p.money - 5
+					p.addInventory(Bottes)
+				}
+			} else if p.money < 5 {
+				fmt.Println("Vous n'avez plus assez d'agent!!")
+				for c := 0; c < p.money; c++ {
+					if p.money < 5 && c > 1 {
+						fmt.Print("Il te reste ") // donne le nombre de po restant du joueur
+						fmt.Print(c)
+						fmt.Print(" pièces d'or")
+
+					} else if p.money < 5 && c <= 1 {
+						fmt.Print("Il te reste ")
+						fmt.Print(c)
+						fmt.Print(" pièce d'or")
+					}
+				}
+			} else {
+				fmt.Println("Il vous manque un item pour craft")
+			}
+		}
+	}
+}
 func main() {
 	var p1 personnage
 	p1.init("jackouille", "fripouille", 150, 10, 1, []string{"popovie", "poison", "popovie", "popovie", "popovie", "popovie", "popovie", "popovie"}, "coup de point", 100)
 	var p2 personnage
 	p2.init("Cristian ", "Cristian", 150, 1, 1, []string{"poison", "poison", "poison", "poison", "poison", "poison", "poison", "popovie"}, "coup de point", 100)
-	p1.poison()
-	p1.pnj(5)
-	p1.displayInfo()
 	//p2.displayInfo()
 	//p2.poison()
 	fmt.Println()
